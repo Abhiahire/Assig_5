7d9d2178dda82198f7c90667288762e90d5a2bb5
Abhiahire <168887094+Abhiahire@users.noreply.github.com>
2025-05-07 20:41:03 +0530

    Update HelloJava.java

--- HelloJava.java
+++ HelloJava.java
@@ -2,6 +2,6 @@ public class HelloJava {
          public static void main(String[] args) {
              String name = "AbhiAhire"; 
              System.out.println("Hello, " + name + "!");
-             System.out.println("Welcome to C-DAC")
+             System.out.println("Welcome to C-DAC");
          }
      }
