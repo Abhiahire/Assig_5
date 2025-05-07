7f45d40be7e82183383e0a3175077b9f491ce4ed
abhiahire <abhiahire1126@gmail.com>
2025-05-08 01:54:18 +0530

    hellowolrd

--- HelloJava.java
+++ HelloJava.java
@@ -1,4 +1,4 @@
-public class HelloStudent {
+public class HelloJava {
          public static void main(String[] args) {
              String name = "AbhiAhire"; 
              System.out.println("Hello, " + name + "!");
